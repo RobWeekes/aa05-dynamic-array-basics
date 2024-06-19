a1ad53522807a403c4ec020fd8b2d08991d7d051
Robert Weekes <weekes.robert@gmail.com>
2024-06-19 16:24:03 -0700

    passing 3/4

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,14 +1,15 @@
 class DynamicArray {
 
-  constructor(defaultSize=4) {
-
-    // Your code here 
-
+  constructor(defaultSize = 4) {
+      this.data = [];
+      this.capacity = defaultSize;
+      this.length = 0;
+      this.data.length = defaultSize;
   }
 
   read(index) {
-
-    // Your code here 
+      if(index < 0 || typeof index !== 'number') throw new Error('index must be non-negative integer');
+      return this.data[index];
   }
 
   unshift(val) {
@@ -17,6 +18,16 @@ class DynamicArray {
   }
 
 }
+
+// local testing
+
+dynamicArr = new DynamicArray(8);
+
+dynamicArr.data[0] = 99; //  = 99;
+dynamicArr.length = 1 //  = 1;
+console.log(dynamicArr);
+console.log(dynamicArr.read(0)) // 99
+console.log(dynamicArr.read(1)) // undef
 
 
 module.exports = DynamicArray;
