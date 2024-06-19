06ed10bc4bc87eef77a27ade708930d12de54487
Brandon McCarthy <Bmac96.dev@gmail.com>
2024-06-19 19:30:42 -0400

    Solved all 4 test specs

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,22 +1,26 @@
 class DynamicArray {
+    constructor(defaultSize = 4) {
+        this.data = [];
+        this.capacity = defaultSize;
+        this.length = 0;
+        this.data.length = defaultSize;
+    }
 
-  constructor(defaultSize = 4) {
-      this.data = [];
-      this.capacity = defaultSize;
-      this.length = 0;
-      this.data.length = defaultSize;
-  }
+    read(index) {
+        if (index < 0 || typeof index !== "number")
+            throw new Error("index must be non-negative integer");
+        return this.data[index];
+    }
 
-  read(index) {
-      if(index < 0 || typeof index !== 'number') throw new Error('index must be non-negative integer');
-      return this.data[index];
-  }
+    unshift(val) {
+        for (let i = this.length; i > 0; i--) {
+            this.data[i] = this.data[i - 1];
+        }
 
-  unshift(val) {
-
-    // Your code here 
-  }
-
+        // Add value to front
+        this.data[0] = val;
+        this.length++;
+    }
 }
 
 // local testing
@@ -24,10 +28,9 @@ class DynamicArray {
 dynamicArr = new DynamicArray(8);
 
 dynamicArr.data[0] = 99; //  = 99;
-dynamicArr.length = 1 //  = 1;
+dynamicArr.length = 1; //  = 1;
 console.log(dynamicArr);
-console.log(dynamicArr.read(0)) // 99
-console.log(dynamicArr.read(1)) // undef
-
+console.log(dynamicArr.read(0)); // 99
+console.log(dynamicArr.read(1)); // undef
 
 module.exports = DynamicArray;
